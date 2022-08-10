2304277f4985e0b353ba557be5258a1f7b083f60
byack <byack-hkc@foxmail.com>
2022-08-10 18:11:24 +0800

    update some function

--- Sentiment_classification/emo_utils.py
+++ Sentiment_classification/emo_utils.py
@@ -1,5 +1,6 @@
 import csv
 import emoji
+import torch
 import numpy as np
 
 
@@ -16,6 +17,7 @@ emoji_dictionary = {
 # 通过 label 转换为对应的 emoji 表情
 def label_to_emoji(label):
     return emoji.emojize(emoji_dictionary[str(label)])
+
 
 # 读取 csv 文件中的数据，转为 array 返回
 def read_csv(filename):
@@ -36,6 +38,24 @@ def label_to_one_hot(label, c):
     return np.eye(c)[label]
 
 
+# 将词转换为词向量存储为 字典 方便后续使用
+def read_glove_vecs(glove_file):
+    word_to_vec_map = {}
+    with open(glove_file, 'r',  encoding='utf-8') as f:
+        line = f.readline()
+        while line:
+            line = line.strip().split()
+            word_to_vec_map[line[0]] = torch.tensor(list(map(float, line[1:])), dtype=torch.float32)
+            line = f.readline()
+    
+    return word_to_vec_map
+
+# 使用 numpy 简单实现 softmax
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+
 if __name__ == "__main__":
-    x = label_to_emoji(4)
-    print(x)
+    w_t_i, i_t_w, w_t_v_m = read_glove_vecs("emo_data/glove.6B.50d.txt")
+    print(w_t_i, "\n", i_t_w, "\n", w_t_v_m)
